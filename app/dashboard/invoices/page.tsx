ca36c2d15b4e9408a1124a5bd6cf852c67d257fc
wsz27285 <wsz27285@gmail.com>
2026-04-07 20:28:20 +0800

    streaming

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,6 @@
-export default function page(){
+export default async function page(){
+    await new Promise((resolve) => setTimeout(resolve, 100));
     return <>
+    
     <h1>发票管理</h1></>
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,11 +7,15 @@ import {
   LatestInvoiceRaw,
   Revenue,
 } from './definitions';
+
 import { formatCurrency } from './utils';
+
+import { unstable_noStore as noStore } from 'next/cache';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
+  noStore()
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -31,6 +35,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -51,6 +56,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore()
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -90,6 +96,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore()
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -122,6 +129,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore()
   try {
     const data = await sql`SELECT COUNT(*)
     FROM invoices
@@ -143,6 +151,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore()
   try {
     const data = await sql<InvoiceForm[]>`
       SELECT
@@ -168,6 +177,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore()
   try {
     const customers = await sql<CustomerField[]>`
       SELECT
@@ -185,6 +195,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore()
   try {
     const data = await sql<CustomersTableType[]>`
 		SELECT
